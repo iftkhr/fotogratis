20ef309b3e9c4776fd1ed9a25247c485290b1786
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-03-20 22:30:13 +0530

    Add search bar

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// components
+
+import SearchBar from "./components/SearchBar/SearchBar";
+
 class App extends React.Component {
 	render() {
 		return (
 			<div className="main">
 				<div className="logo">Fotogratis</div>
 				<div className="tagline">Free photos & videos.</div>
-				<div className="searchbar">Search</div>
-				<div className="filters">Filters</div>
+				<div className="searchbar"><SearchBar/></div>
 				<div className="photos">Photos</div>
 			</div>
 		);
