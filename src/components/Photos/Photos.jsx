75da1486075058e9d8d48f7900d9d61ff7c0fe92
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-03-22 23:19:41 +0530

    Add pexels API

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -7,13 +7,23 @@ import Photo from '../Photo/Photo';
 
 class Photos extends React.Component {
 	render() {
-		return (
-			<div className="photos-container">
-				<div className="photo">
-					<Photo />
+		if (this.props.error) {
+			return <div className="photos-container">Error!</div>;
+		} else if (!this.props.load) {
+			return <div className="photos-container">Loading...</div>;
+		} else {
+			return (
+				<div className="photos-container">
+					{console.log(this.props.photos)}
+
+					{this.props.photos.map((photo) => (
+						<div className="photo" key={photo.id}>
+							<Photo source={photo.src} />
+						</div>
+					))}
 				</div>
-			</div>
-		);
+			);
+		}
 	}
 }
 
